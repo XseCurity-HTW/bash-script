ba38e37e875a184e82f6056e97889137d6c12cda
XseCurity-HTW <70092703+XseCurity-HTW@users.noreply.github.com>
2020-09-03 19:22:55 +0530

    Add files via upload

---
+++ 1st.sh
@@ -0,0 +1,26 @@
+clear
+echo '
+                
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X
+X  \...X     @#%,.@   ----------------    @ @ 00 0 xxxxxxxxx
+ X# \.X        @#%,.@   Infect v1.1      @#%,.@        
+                @#%,.@              @#%,.@          
+                  @#%,.@          @#%,.@            
+                     @#%,.@      @#%,.@             
+                       @#%.,@  @#%,.@              
+                         White hat Hacker56
+'
+echo "enter your name: "
+read name
+echo "Hello $name how are you?  To become an ethical hacker watch this video $(tput setaf 1)https://youtu.be/IDnPX3JTq0A"
